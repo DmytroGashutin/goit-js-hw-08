9ee23f5e8a98e145720ec20ae2428a360ba1dc2e
Dmytro Gashutin <dm.gashutin@gmail.com>
2022-02-22 00:23:06 +0200

    tasks, pull try 2

---
+++ src/js/03-feedback.js
@@ -0,0 +1,42 @@
+
+import throttle from 'lodash.throttle';
+
+
+const FORM_KEY = 'feedback-form-state';
+
+const formRef = document.querySelector('.feedback-form');
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+inLocalStorage();
+
+function onFormInput(event) {
+    localStorage.setItem(FORM_KEY, JSON.stringify(createStatusObject()));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(createStatusObject());
+    formRef.reset();
+    localStorage.removeItem(FORM_KEY);
+    
+}
+function createStatusObject() {
+    return {
+        email: formRef.elements.email.value,
+        message: formRef.elements.message.value,
+    };
+    
+}
+
+function inLocalStorage() {
+    let currentFieldStatus;
+    try {
+        currentFieldStatus = JSON.parse(localStorage.getItem(FORM_KEY));
+        formRef.elements.email.value = currentFieldStatus.email;
+        formRef.elements.message.value = currentFieldStatus.message;
+    } catch (error) {
+
+    };
+}
