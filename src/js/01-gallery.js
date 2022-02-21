156a8660af5e171aa73704bc43a5a23e96458335
Dmytro Gashutin <dm.gashutin@gmail.com>
2022-02-21 23:20:29 +0200

    tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const imageMarkup = createImagesMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+
+console.log(createImagesMarkup(galleryItems));
+
+function createImagesMarkup(items) {
+  
+    return items.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__item" href="${original}">
+                <img 
+                class="gallery__image"
+                src="${preview}" 
+                alt="${description}" />
+            </a>
+        </div>
+        `;
+    }).join('');
+
+}
+
+
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
